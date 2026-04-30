87f0d87d3e009f08beb6de804cba2b3692bd386d
Saanidhya Sharma <saanidhyasharma.10f.33@gmail.com>
2026-04-30 11:46:45 +0530

    Comment out debug Home component

--- app/page.js
+++ app/page.js
@@ -72,13 +72,13 @@ export default function HomePage() {
 }    
 
 
-"use client";
+// "use client";
 
-export default function Home() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>TaskHive is Live 🚀</h1>
-      <p>If you see this, routing is working.</p>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div style={{ padding: "40px" }}>
+//       <h1>TaskHive is Live 🚀</h1>
+//       <p>If you see this, routing is working.</p>
+//     </div>
+//   );
+// }
